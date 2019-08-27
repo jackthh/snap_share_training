52d762458eb426aa47f29b037d7e672fd700619c
jackthh <jack.truonghoanghiep@gmail.com>
2019-08-27 17:31:31 +0700

    + Implement login screen UI + Change launcher icon

---
+++ app/src/main/java/com/teamandroid/snapshare/ui/main/LogInActivity.java
@@ -0,0 +1,17 @@
+package com.teamandroid.snapshare.ui.main;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.teamandroid.snapshare.R;
+
+public class LogInActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+    }
+}
