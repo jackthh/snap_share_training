c4c241a2457c90ba2828f2f237e7e8a03fc59570
jackthh <jack.truonghoanghiep@gmail.com>
2019-09-06 13:10:32 +0700

    Processing fetching

--- app/src/main/java/com/teamandroid/snapshare/ui/main/MainActivity.java
+++ app/src/main/java/com/teamandroid/snapshare/ui/main/MainActivity.java
@@ -2,15 +2,18 @@ package com.teamandroid.snapshare.ui.main;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.teamandroid.snapshare.R;
 import com.teamandroid.snapshare.ui.main.home.HomeFragment;
+import com.teamandroid.snapshare.ui.main.profile.ProfileFragment;
 import com.teamandroid.snapshare.ui.post.PostActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,23 +24,27 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     //TODO
-                    return true;
+                    break;
                 case R.id.navigation_search:
                     //TODO
-                    return true;
+                    break;
                 case R.id.navigation_add_post:
                     openPostActivity();
-                    return true;
+                    break;
                 case R.id.navigation_account:
+                    Log.i("FRAGMENT SWITCH", "Launched profile");
+                    fragment = ProfileFragment.newInstance();
                     //TODO
-                    return true;
+                    break;
             }
-            return false;
+            return loadFragment(fragment);
         }
     };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,8 +59,23 @@ public class MainActivity extends AppCompatActivity {
                 .commit();
     }
 
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.content_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+
     private void openPostActivity() {
         Intent intent = new Intent(this, PostActivity.class);
         startActivity(intent);
     }
+
+
 }
